4573ad33b0c26e903af4c62f86d67af682114149
IanDias10 <diasian8@hotmail.com>
2022-01-25 21:51:35 -0300

    Finalização do app de simulação de partida

--- app/src/main/java/com/example/appsimulator/ui/MainActivity.java
+++ app/src/main/java/com/example/appsimulator/ui/MainActivity.java
@@ -1,18 +1,24 @@
 package com.example.appsimulator.ui;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.os.Bundle;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.appsimulator.R;
 import com.example.appsimulator.data.MatchesApi;
 import com.example.appsimulator.databinding.ActivityMainBinding;
 import com.example.appsimulator.domain.Match;
+import com.example.appsimulator.ui.adapter.MatchesAdapter;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.util.List;
+import java.util.Random;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -23,6 +29,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private MatchesApi matchesApi;
+    private MatchesAdapter matchesAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -47,30 +54,54 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setupMatchesList() {
+        binding.rvMatches.setHasFixedSize(true);
+        binding.rvMatches.setLayoutManager(new LinearLayoutManager(this));
+        findMatchesFromApi();
+    }
+
+    private void setupMatchesRefresh() {
+        binding.srlMatches.setOnRefreshListener(this::findMatchesFromApi);
+    }
+
+    private void setupFloatingActionButton() {
+        binding.fsbSimulate.setOnClickListener(view -> {
+            view.animate().rotationBy(360).setDuration(500).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    Random random = new Random();
+                    for (int i = 0; i < matchesAdapter.getItemCount(); i++) {
+                        Match match = matchesAdapter.getMatches().get(i);
+                        match.getHomeTeam().setScore(random.nextInt(match.getHomeTeam().getStars() + 1));
+                        match.getAwayTeam().setScore(random.nextInt(match.getAwayTeam().getStars() + 1));
+                        matchesAdapter.notifyItemChanged(i);
+                    }
+                }
+            });
+        });
+    }
+
+    private void findMatchesFromApi() {
+        binding.srlMatches.setRefreshing(true);
         matchesApi.getMatches().enqueue(new Callback<List<Match>>() {
             @Override
             public void onResponse(Call<List<Match>> call, Response<List<Match>> response) {
                 if (response.isSuccessful()) {
                     List<Match> matches = response.body();
-                    Log.i("SIMULATOR", "Deu tudo certo! Partidas = " + matches.size());
+                    matchesAdapter = new MatchesAdapter(matches);
+                    binding.rvMatches.setAdapter(matchesAdapter);
                 } else {
                     showErrorMessage();
                 }
+                binding.srlMatches.setRefreshing(false);
             }
 
             @Override
             public void onFailure(Call<List<Match>> call, Throwable t) {
+                t.printStackTrace();
                 showErrorMessage();
+                binding.srlMatches.setRefreshing(false);
             }
         });
-    }
-
-    private void setupMatchesRefresh() {
-        //TODO: Atualizar as partidas na ação de swipe.
-    }
-
-    private void setupFloatingActionButton() {
-        //TODO: Criar eventos de click e simulação de partidas.
     }
 
     private void showErrorMessage() {
